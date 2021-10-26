31947ff78fd8d8de394d1380b7d849a47f93a232
annpermi <perminova.anna85@gmail.com>
2021-10-26 15:20:08 -0400

    Test and code button click

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+/* test("renders learn react link", () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  const linkElement = screen.getByText("Learn React");
+  const linkElement = screen.getByRole("link", { name: /learn react/i });
+  // const linkElement = screen.getByText("Learn React");
   expect(linkElement).toBeInTheDocument();
+}); */
+
+//1. Write initial test
+test("button has correct initial color", () => {
+  //2. Add render method for component, access to virtual DOM
+  render(<App />);
+
+  //find an element with role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  //expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  //click button
+  //fireEvent - interact with dom elements
+  fireEvent.click(colorButton);
+
+  //expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  //expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe("Change to red");
+});
+
+test("button turns blue when click", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
 });
